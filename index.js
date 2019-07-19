73b8a9faf9fafae4a1040a9da5bd1bc274443aed
Kai Laptop <kaijohan428@gmail.com>
2019-07-19 17:52:20 +0200

    cleanup

--- index.js
+++ index.js
@@ -22,12 +22,18 @@ var myGenres = [
 app.use(express.json());
 
 // GET
-app.get('/genres', (req, res) => {
+app.get('/api/genres', (req, res) => {
     res.send(myGenres);
+});
+app.get('/api/genres/:id', (req, res) => {
+    const genre = myGenres.find(item => item.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send('Genre with provided ID not found');
+
+    res.send(genre);
 });
 
 // POST
-app.post('/genres', (req, res) => {
+app.post('/api/genres', (req, res) => {
     const { error } = validateGenre(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
@@ -35,14 +41,14 @@ app.post('/genres', (req, res) => {
         id: myGenres.length + 1,
         genre: req.body.genre
     };
-    myGenres.push(genre);
 
+    myGenres.push(genre);
     res.send(genre);
 
 });
 
 // PUT
-app.put('/genres/:id', (req, res) => {
+app.put('/api/genres/:id', (req, res) => {
     const genreObj = myGenres.find(item => item.id === parseInt(req.params.id));
     if(!genreObj) return res.status(404).send('Genre with provided ID not found');
 
@@ -50,17 +56,17 @@ app.put('/genres/:id', (req, res) => {
     if(error) return res.status(400).send(error.details[0].message)
 
     genreObj.genre = req.body.genre;
-
     res.send(genreObj);
 });
 
 // DELETE
-app.delete('/genres/:id', (req, res) => {
+app.delete('/api/genres/:id', (req, res) => {
     const genreObj = myGenres.find(item => item.id === parseInt(req.params.id));
     if(!genreObj) return res.status(404).send('Genre with provided ID not found');
 
     const index = myGenres.indexOf(genreObj);
     myGenres.splice(index, 1);
+
     res.send(genreObj);
 });
 
