616dc64392b64c19928ce685a50a04ddaad56cdc
Kai Laptop <kaijohan428@gmail.com>
2019-07-25 17:30:45 +0200

    adding movie model

---
+++ models/movie.js
@@ -0,0 +1,54 @@
+/**
+ * Dependencies
+ */
+const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+const {genreSchema} = require('./genre');
+
+/**
+ * Model and Schema
+ */
+const Movie = mongoose.model('Movie', new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255
+    },
+    genre: {
+        type: genreSchema,
+        required: true
+    },
+    numberInStock: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+    },
+    dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+    }
+}));
+
+/**
+ * Functions
+ */
+function validateMovie(movie) {
+    const schema = {
+        title: Joi.string().min(5).max(50).required(),
+        genreId: Joi.string().required(),
+        numberInStock: Joi.number().min(0).required(),
+        dailyRentalRate: Joi.number().min(0).required()
+    };
+    return Joi.validate(movie, schema);
+}
+
+/**
+ * Expots
+ */
+module.exports.Movie = Movie;
+module.exports.joiValidate = validateMovie;
