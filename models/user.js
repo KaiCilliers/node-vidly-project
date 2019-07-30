db199331f5c3b083cb995dc6a8d2987576ee51d9
Kai Laptop <kaijohan428@gmail.com>
2019-07-30 14:39:46 +0200

    user model

---
+++ models/user.js
@@ -0,0 +1,48 @@
+/**
+ * Dependencies
+ */
+const Joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+
+/**
+ * Models with Schemas
+ */
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+}));
+
+/**
+ * Functions
+ */
+function validateUser(user) {
+    const schema = {
+        name: Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(5).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+    return Joi.validate(user, schema);
+}
+
+/**
+ * Exports
+ */
+module.exports.User = User;
+module.exports.joiValidate = validateUser;
