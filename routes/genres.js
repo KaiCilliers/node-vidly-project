b17e971edbae0cca54463cc8fd11b085cef4ef79
Kai Laptop <kaijohan428@gmail.com>
2019-07-31 11:49:15 +0200

    solution to problem

--- routes/genres.js
+++ routes/genres.js
@@ -11,20 +11,6 @@ const {Genre, joiValidate} = require('../models/genre');
 /**
  * GET
  */
-/**
- * Explanation. The anonymous function is not called by you.
- * You are passing a function reference.
- * Express uses the reference and calls it at runtime and passes
- * the values req, res, next to the function.
- * 
- * This is considered called
- * (req, res, next) => {}(val, val2, val3)
- */
-router.get('/anotherpath', (req, res, next) => {
-
-});
-// problem is, you care calling the asyncMiddleware function, and
-// not passing a reference
 router.get('/', asyncMiddleware(async (req, res, next) => {
     const genres = await Genre.find().sort('name');
     res.send(genres);
@@ -88,13 +74,16 @@ router.delete('/:id', [auth, admin], async (req, res) => {
 // Call it a function which returns a new function
 // Returned function is a route handler which Express can call
 // and pass req, res, next arguments to at runtime
-async function asyncMiddleware(handler) { // handler is expected to be an async function
-    // handler is a function (a route handler function so to speak :)
-    try{
-        await handler(req, res);
-    } catch(ex) {
-        next(ex);
-    }
+function asyncMiddleware(handler) { // no longer awaiting a promise, simply returning a function/route handler
+    return async (req, res, next) => {
+        try{
+            await handler(req, res);
+        } catch(ex) {
+            next(ex);
+        }
+    };
+    // Try/catch is now in a single place, no need to repeat it
+    // in each route handler
 }
 
 /**
