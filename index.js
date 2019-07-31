cc559a8292bf128cf335d1fcd9132e6a16203bfc
Kai Laptop <kaijohan428@gmail.com>
2019-07-31 15:04:06 +0200

    routes extracted

--- index.js
+++ index.js
@@ -4,23 +4,17 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error');
 const config = require('config');
 const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const express = require('express');
-const routerGenre = require('./routes/genres');
-const routerCustomer = require('./routes/customers');
-const routerMovie = require('./routes/movies');
-const routerRental = require('./routes/rentals');
-const routerUser = require('./routes/users');
-const routerAuth = require('./routes/auth');
 
 /**
  * Server Connection
  */
 const app = express();
+require('./startup/routes')(app);
 
 /**
  * Error handling
@@ -39,9 +33,6 @@ winston.add(new winston.transports.MongoDB({
     level: 'info'
 }));
 
-const p = Promise.reject(new Error('FAILED badly'));
-p.then(() => console.log('Done'));
-
 /**
  * Environment Varaibles Setup
  */
@@ -56,18 +47,6 @@ if (!config.get('jwtPrivateKey')) {
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'));
-
-/**
- * Middleware
- */
-app.use(express.json());
-app.use('/api/genres', routerGenre);
-app.use('/api/customers', routerCustomer);
-app.use('/api/movies', routerMovie);
-app.use('/api/rentals', routerRental);
-app.use('/api/users', routerUser);
-app.use('/api/auth', routerAuth);
-app.use(error);
 
 /**
  * Listener
