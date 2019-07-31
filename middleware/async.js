276aedb951dded9b1ed5c7049a324eca799717ad
Kai Laptop <kaijohan428@gmail.com>
2019-07-31 11:52:55 +0200

    cleanup solution

---
+++ middleware/async.js
@@ -0,0 +1,12 @@
+/**
+ * Functions and Exports
+ */
+module.exports = function asyncMiddleware(handler) {
+    return async (req, res, next) => {
+        try{
+            await handler(req, res);
+        } catch(ex) {
+            next(ex);
+        }
+    };
+}
--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,7 @@
 /**
  * Dependencies
  */
+const asyncMiddleware = require('../middleware/async');
 const admin = require('../middleware/admin');
 const auth = require('../middleware/auth');
 const express = require('express');
@@ -26,7 +27,7 @@ router.get('/:id', async (req, res) => {
 /**
  * POST
  */
-router.post('/', auth, async (req, res) => {
+router.post('/', auth, asyncMiddleware(async (req, res) => {
     const { error } = joiValidate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
@@ -36,7 +37,7 @@ router.post('/', auth, async (req, res) => {
     
     await genre.save();
     res.send(genre);
-});
+}));
 
 /**
  * PUT
@@ -64,27 +65,6 @@ router.delete('/:id', [auth, admin], async (req, res) => {
 
     res.send(genre);
 });
-
-/**
- * Functions
- */
-// This function has to return a route handler function that
-// looks like (req, res, next)
-// Acts like a factory
-// Call it a function which returns a new function
-// Returned function is a route handler which Express can call
-// and pass req, res, next arguments to at runtime
-function asyncMiddleware(handler) { // no longer awaiting a promise, simply returning a function/route handler
-    return async (req, res, next) => {
-        try{
-            await handler(req, res);
-        } catch(ex) {
-            next(ex);
-        }
-    };
-    // Try/catch is now in a single place, no need to repeat it
-    // in each route handler
-}
 
 /**
  * Exports
