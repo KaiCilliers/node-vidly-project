27aa4dfaf915b4f384430b06b2ce56c27cf5787b
Kai Laptop <kaijohan428@gmail.com>
2019-08-02 14:13:21 +0200

    last logging notes

--- config/log-transports.js
+++ config/log-transports.js
@@ -75,3 +75,17 @@ module.exports = {
         handleExceptions: true,
     }
 }
+/**
+ * Future use
+ * 
+ * level: process.env.ENV === 'development' ? 'debug' : 'error'
+ * 
+ // If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple()
+  }));
+}
+ */
