7f6630bf26f5bf3f174b2b7cad6361f061c71446
Kai Laptop <kaijohan428@gmail.com>
2019-08-05 15:27:17 +0200

    successfully implemented correct solution to a test

--- routes/returns.js
+++ routes/returns.js
@@ -14,12 +14,16 @@ const {Rental} = require('../models/rental');
 router.post('/', async (req, res) => {
     if (!req.body.customerId) return res.status(400).send('customerId not provided');
     if (!req.body.movieId) return res.status(400).send('movieId not provided');
+    
     // Access id in a sub document
     const rental = await Rental.findOne({
         'customer._id': req.body.customerId,
         'movie._id': req.body.movieId
     });
     if (!rental) return res.status(404).send('Rental not found');
+    
+    if (rental.dateReturned) return res.status(400).send('rental already processed');
+    
     res.status(401).send('Unauthorised');
 });
 
